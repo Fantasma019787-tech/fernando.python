6af2d18dbe03aa309ce3644474816fa588cb9a93
fantasma019787 <fernandoelprincipe08@gmail.com>
2026-04-17 11:28:55 -0400

    practica de for

--- dfdgd.py
+++ dfdgd.py
@@ -1,1 +1,12 @@
-print("pene")
+vocales=0
+consonantes=0
+nombre=input("ingrese su nombre ")
+for i in nombre:
+    if i in "aeiouAEIOU":
+       vocales=vocales+1
+    elif i==" ":
+       ""
+    else:
+     consonantes=consonantes+1
+print("el numero de vocales de su nombre es:", vocales)
+print("el numero de consonantes de su nombre es:", consonantes)
