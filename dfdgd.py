255062e856d59bbef50535f754c043cc76249249
Fantasma019787 <fernandoelprincipe08@gmail.com>
2026-04-24 11:54:33 -0400

    primer commit

--- dfdgd.py
+++ dfdgd.py
@@ -5,15 +5,136 @@
 #else:
 #   print("clave incorrecta")
 
-# print("ingrese su nombre")
-# nombre=input
 
-#nombre=print("usuario correcto")
-#if 4<=len(nombre)<=10:
-#else :
+# print("ingrese su nombre")
+# name=input
+# if 4<=len(name)<=10:
+#   name=print("usuario correcto")
+# else :
 #    print("usuario fuera de rango")
 
 
 #pin=int(input("ingrese un numero de 4 digios"))
 
+# op=0
+# total=0
+# while op!=4:
+#  print("1.- PC Ryzen $800.000")
+#  print("2.- LGTV 55 pulgadas")
+#  print("3.- Parlante JBL Pure Sound $90.000")
+#  print("4.- salir")
+#  print("seleccione una opcion")
+#  op=int(input())
+#  match op:
+#     case 1:
+#         print("tiene que pagar ", 800.000*1.19)
+#         total+=800000*1.19
+#     case 2:
+#         print("tiene que pagar ",450.000*1.19)
+#         total+=450000*1.19
+#     case 3:
+#         print("tiene que pagar ",90.000*1.19)
+#         total+=90000*1.19
+#     case 4:
+#         print("saliendo")
+#         print(f"el total a pagar con iva es {round(total,2)}")
+#     case _:
+#         print("opcion invalida")
 
+
+# cont=1
+# while cont<=3:
+#     print(f"contador {cont}")
+#     cont+=1
+
+
+# pin=3535
+# code=int(input("ingrese su pin"))
+# while pin!=code:
+#     print("error intente nuevamente")
+#     code=int(input("ingrese su pin"))
+# print("pin correcto!")
+
+# num=int(input("ingrese un numero"))
+# c=1
+# while c<=10:
+#     print(f"{num}x{c}={num*c}")
+  
+
+# def calculadora():op=0
+
+
+
+# def pin():
+#     pin=3535
+#     code=int(input("ingrese su pin"))
+#     while pin!=code:
+#         print("error intente nuevamente")
+#         code=int(input("ingrese su pin"))
+#     print("pin correcto!")
+
+
+# def calculadora():
+#     op=0
+#     while op!=5: 
+#         print("1.- suma")
+#         print("2.- resta")
+#         print("3.- multiplicacion")
+#         print("4.- divicion")
+#         print("5.- salir")
+#         print("seleccione una opcion")
+#         op=int(input("seleccione una operacion"))
+#         match op:
+#             case 1:
+#              n1=int(input("ingrese un numero: "))
+#              n2=int(input("ingrese otro numero"))
+#              print(f"el resultado es {n1+n2}")
+#             case 2:
+#              n1=int(input("ingrese un numero: "))
+#              n2=int(input("ingrese ottro numero"))
+#              print(f"el resultado es {n1-n2}")
+#             case 3:
+#              n1=int(input("ingrese un numero: "))
+#              n2=int(input("ingrese ottro numero"))
+#              print(f"el resultado es {n1*n2}")
+#             case 4:
+#              n1=int(input("ingrese un numero: "))
+#              n2=int(input("ingrese ottro numero"))
+#              print(f"el resultado es {n1/n2}")
+#             case 5:
+#              print("salir")
+#             case _:
+#              print("opcion invalida")
+
+
+# def tablaM():
+#     c=1
+#     num=int(input("ingrese un numero: "))
+#     while c<=10:
+#         print(f"{num}x{c}={num*c}")
+#         c += 1
+
+
+
+# op=0
+# total=0
+# while op!=4:
+#     print('''menu
+#         1.- calculadora
+#         2.- tablaM
+#         3.- pin
+#         4.- salir
+#         seleccione una opcion''')
+#     op=int(input())
+#     match op:
+#        case 1:
+#           calculadora()
+#        case 2:
+#           tablaM()
+#        case 3:
+#           pin()
+#        case 4:
+#           print("salir")
+#           print(f"intente más tarde")
+#        case _: 
+#           print("opcion invalida")
