d9b524c5ebe528aa96514dc30e252b24d2508949
Fantasma019787 <fernandoelprincipe08@gmail.com>
2026-05-04 23:01:02 -0400

    ejercicios clase casa

---
+++ ejercicios.py
@@ -0,0 +1,179 @@
+
+# num_ticket=int(input("bienvenido, cual es el numero de su ticket: "))
+# if num_ticket>= 1000 and num_ticket<=8500:
+#     print("ticket valido")
+#     print("ingrese su edad: ")
+#     edad=int(input("ingrese su edad"))
+#     if edad>= 15:
+#         print("puede pasar al concierto")
+#     else:
+#         print("no puede pasar")
+
+# num1=int(input("ingrese un numero: "))
+# for i in range(10):
+#     print(f"{num1} x {i+1} = {num1*(i+1)}")
+
+# cont=0
+# while cont<3:
+#     print(f"contador {cont}")
+#     cont+=1
+
+# for i in range(1):
+#     print("ingrese las notas del alumno ")
+#     suma=0
+#     for k in range(3):
+#         n=int(input())
+#         suma=suma+n
+#     prom=suma/3
+#     print(f"el promedio de notas es {prom}")
+#     if prom>40:
+#         print("el alumno aprobó")
+#     else:
+#         print("waja aweonao")
+
+# c_num=0
+# num=int(input("ingrese un numero: "))
+# for i in range(1,11):
+#     c_num=+1
+#     print(f"{num} x {i} = {num*i}")
+
+# num=int(input("ingrese un numero: "))
+# cont=1
+# while c<=10:
+#         print(f"{num}x{c}={num*c}")
+#         cont+=1
+
+# edad=int(input("ingrese su edad: "))
+
+# if edad>=18 and edad<100 :
+#     print("eres mayor de edad")
+# elif edad<18 and edad>=1:
+#     print("eres menor de edad")
+# else:
+#     print("WHERE IS OMNI-MAN")
+
+# op =0
+# totalap=0
+# while op !=4:
+#     print("bienvenido")
+#     print("1.- manzanas $500")
+#     print("2.- naranjas $600")
+#     print("3.- sandia $2000")
+#     print("4.-salir")
+#     op=int(input("elija una opcion: "))
+#     match op:
+#         case 1:
+#             print("usted compro manzanas")
+#             totalap+=500
+#         case 2:
+#             print("usted compro naranjas")
+#             totalap+=600
+#         case 3:
+#             print("usted compro sandia")
+#             totalap+=2000
+#         case 4: 
+#             print("saliendo del sistema")
+#             print("gracias por su compra")
+#         case _:
+#             print("opcion invalida")
+# print(f"el total a pagar es {totalap}")
+
+
+# op=0
+# while op !=5:
+#     print("1.-suma")
+#     print("2.-resta")
+#     print("3.-multiplipicar")
+#     print("4.-dividir")
+#     print("5.- salir")
+#     op=int(input("que quiere hacer: "))
+#     match op:
+#         case 1:
+#           def sumar():
+#             num1=int(input("ingrese su numero: "))
+#             num2=int(input("ingrese otro numero: "))
+#             suma=num1+num2
+#             print(f"el resultado es{suma}")
+#           sumar()
+#         case 2:
+#           def resta():
+#             num1=int(input("ingrese su numero: "))
+#             num2=int(input("ingrese otro numero: "))
+#             resta=num1-num2
+#             print(f"el resultado es{resta}")
+#           resta()
+#         case 3:
+#           def multi():
+#             num1=int(input("ingrese su numero: "))
+#             num2=int(input("ingrese otro numero: "))
+#             multi=num1*num2
+#             print(f"el resultado es{multi}")
+#           multi()
+#         case 4:
+#           def div():
+#             num1=int(input("ingrese su numero: "))
+#             num2=int(input("ingrese otro numero: "))
+#             div=num1//num2
+#             print(f"el resultado es{div}")
+#           div()
+#         case 5:
+#           print("saliendo")
+#         case _:
+#           print("opcion incorrecta")
+
+
+
+
+
+
+
+# def sumar():
+#     num1=int(input("ingrese su numero"))
+#     num2=int(input("ingrese otro numero"))
+#     suma=num1+num2
+#     print(f"el resultado es{suma}")
+# sumar()
+
+# def dividir():
+#    num1=int(input("ingrese su numero"))
+#    num2=int(input("ingrese otro numero")) 
+#    div=num1/num2
+#    print(f"el resultado es{div}")
+# dividir()
+
+
+import random
+# j1=input("ingrese su nombre: ")
+# j2=input("ingrese su nombre: ")
+# j3=input("ingrese su nombre: ")
+
+# dado1=random.randint(1,6)
+# dado2=random.randint(1,6)
+# dado3=random.randint(1,6)
+# if dado1>dado2 and dado1>dado3:
+#     print(f"el ganador es {j1} con {dado1} puntos ")
+#     print(f"la puntuacion de {j2} es {dado2}")
+#     print(f"la puntuacion de {j3} es {dado3}")
+# elif dado2>dado3:
+#         print(f"el ganador es {j2} con {dado2} puntos ")
+#         print(f"la puntuacion de {j1} es {dado1}")
+#         print(f"la puntuacion de {j3} es {dado3}")
+# else:
+#         print(f"el ganador es {j3} con {dado3} puntos ")
+#         print(f"la puntuacion de {j2} es {dado2}")
+#         print(f"la puntuacion de {j1} es {dado1}")
+
+alacran=100
+subcerro=100
+turno=1
+while alacran>0 and subcerro>0:
+    print(f" estamos en el turno {turno}")
+    golpe1=random.randint(7,15)
+    golpe2=random.randint(7,15)
+    subcerro -=golpe1
+    alacran -=golpe2
+    turno+=1
+    if alacran>subcerro:
+        print("alacran gana el mortal kumbias")
+    else:
+        print("subcerro gana el mortal kumbias")
---
+++ for.py
@@ -0,0 +1,37 @@
+# num=int(input("ingrese su numero: "))
+# if num<=-1:
+#     print("este es un numero negativo")
+# elif num == 1:
+#     print(" este numero es positivo")
+# else:
+#     print("este numero es 0")
+
+# edad=int(input("ingrese su edad: "))
+# if edad>=18:
+#     print("eres un adulto ")
+# elif edad <=17 and edad>=1:
+#     print("eres un cabro chico ")
+# else:
+#     print("mentiroso conchetumare")
+
+# num1=int(input("ingrese un numero: "))
+# for i in range (10):
+#     print(f"{num1} x {i+1} = {num1*(i+1)}")
+
+# palabra=input("escriba una palabra: ")
+# contador=0
+# for letra in palabra:
+#     print(letra)
+#     contador+=1
+# print(f"la palabra tiene {contador} letras. ")
+
+# i=1
+# suma=0
+# while i <=5: 
+#  suma+=i
+#  i+=1
+# print(suma)
+
+nom=input("ingrese su nombre: ")
+ap=input("ingrese su apellido: ")
+print(nom.capitalize(), ap.capitalize())
